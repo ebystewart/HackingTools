95782f43aa01d5ecbd6fd70ac6edff0056199d24
Eby Stewart <ebyystewart@gmail.com>
2025-10-05 23:56:36 +0530

    MAC changer

---
+++ 1_MAC_Changer/mac_changer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+import re
+
+#Test: python3 mac_changer.py -i eth0 -m 11:22:33:33:22:11
+#      python3 mac_changer.py -i enp0s3 -m 11:22:33:33:22:11
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface for which MAC is to be changed")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    #return parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface; use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a MAC; use --help for more info")
+    return options
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    #print(ifconfig_result)
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        printf("[-] Could not read MAC address")
+
+options = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
+
+change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+
+if current_mac == options.new_mac:
+    printf("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address change failed")
--- 1_MAC_Changer/mac_demo.py
+++ 1_MAC_Changer/mac_demo.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
+import re
+
+#Test: python3 mac_demo.py -i eth0 -m 11:22:33:33:22:11
 
 #interface = "eth0"
 #new_mac = "11:22:33:44:55:66"
@@ -8,12 +12,33 @@ import subprocess
 #subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
 #subprocess.call("ifconfig " + interface + " up", shell=True)
 
-interface = input("interface > ") # use raw_input() for python2
-new_mac = input("New MAC > ")
+#interface = input("interface > ") # use raw_input() for python2
+#new_mac = input("New MAC > ")
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface for which MAC is to be changed")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    #return parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface; use --help for more info")
+    elif not options.new_mac:
+        parse.error("[-] Please specify a MAC; use --help for more info")
+    return options
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+
+#interface = options.interface
+#new_mac = options.new_mac
+#(options, arguments) = get_arguments()
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
+
 
