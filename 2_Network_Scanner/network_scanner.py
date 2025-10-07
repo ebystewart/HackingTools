45e40acdd8b0e9d43981c5b7bbd5040955cb4a2f
Eby Stewart <ebyystewart@gmail.com>
2025-10-07 23:59:19 +0530

    network scanner - minor changes

--- 2_Network_Scanner/network_scanner.py
+++ 2_Network_Scanner/network_scanner.py
@@ -10,7 +10,12 @@ def scan(ip):
     #broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") # this step is not required as dst is of type broadcast by default
 
     arp_request_broadcast = broadcast/arp_request
-    (answered, unanswered) = scapy.srp(arp_request_broadcast, timeout=1)
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+
+    for element in answered_list:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("------------------------------------------------------------------")
 
     #print(arp_request.summary())
     #scapy.ls(scapy.ARP()) # This is used to list the supported parameters of ARP()
@@ -22,11 +27,11 @@ def scan(ip):
 
     #print(arp_request_broadcast.summary())
     #arp_request_broadcast.show()
-    
-    #print(answered.summary())
-    #print(unanswered.summary())
+
+    #print(answered_list.summary())
+    #print(unanswered_list.summary())
 
 
 # use route -n command in linux to find the gateway IP
 #scan("10.0.2.2")
-#scan("10.0.2.2/24")
+scan("10.0.2.2/28")
